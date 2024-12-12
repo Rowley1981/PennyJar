5e39bd5e819e55d92e22cb30cbea7311624445e3
Chris Rowley <Rowleychris@icloud.com>
2024-12-12 08:40:22 +0000

    Initial Commit

---
+++ PennyJar/PennyJarApp.swift
@@ -0,0 +1,17 @@
+//
+//  PennyJarApp.swift
+//  PennyJar
+//
+//  Created by Chris Rowley on 12/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct PennyJarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
